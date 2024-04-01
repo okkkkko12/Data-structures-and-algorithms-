d73adac90dedbb10cf1145e1009c7c91b4c9b35e
FarahAlhammadi <161212667+FarahAlhammadi@users.noreply.github.com>
2024-04-01 07:09:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import datetime
 
-class User:
+class User:    # Creating user class to represent the user of the system
   def __init__(self, employee_id, name, role):
-    self.employee_id = employee_id
-    self.name = name
-    self.role = role
+      self.employee_id = employee_id
+      self.name = name               # User name
+      self.role = role               # Any kind of rule(doctor, receptionist)
+    
+class authorized_user:    # A class to manage and handle user authentication
+    users = []                       # A class variable to hold the system users list
+
+    @classmethod
+    def add_user(cls, user):
+        cls.users.append(user)       # Will add new user to the list
+
+    @classmethod
+    def authenticate(cls, employee_id, name):
+        # Verify a user's identity using their name and employee ID.
+        for user in cls.users:
+            if user.employee_id == employee_id and user.name == name:
+                return user          # If found, it's will return user object
+        return None                  # Will return None if authentication fails
